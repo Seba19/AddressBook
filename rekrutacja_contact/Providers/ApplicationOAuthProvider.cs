9a58564fd6a7f98422bd8d3d83baea76b5e86319
Sebastian Langa <l.seba1991@gmail.com>
2016-03-14 09:29:11 +0100

    fixes

--- rekrutacja_contact/Providers/ApplicationOAuthProvider.cs
+++ rekrutacja_contact/Providers/ApplicationOAuthProvider.cs
@@ -39,15 +39,15 @@ namespace rekrutacja_contact.Providers
                 return;
             }
 
-            ClaimsIdentity oAuthIdentity = await user.GenerateUserIdentityAsync(userManager,
-               OAuthDefaults.AuthenticationType);
-            ClaimsIdentity cookiesIdentity = await user.GenerateUserIdentityAsync(userManager,
-                CookieAuthenticationDefaults.AuthenticationType);
+        //    ClaimsIdentity oAuthIdentity = await user.GenerateUserIdentityAsync(userManager,
+          //     OAuthDefaults.AuthenticationType);
+            //ClaimsIdentity cookiesIdentity = await user.GenerateUserIdentityAsync(userManager,
+              //  CookieAuthenticationDefaults.AuthenticationType);
 
-            AuthenticationProperties properties = CreateProperties(user.UserName);
-            AuthenticationTicket ticket = new AuthenticationTicket(oAuthIdentity, properties);
-            context.Validated(ticket);
-            context.Request.Context.Authentication.SignIn(cookiesIdentity);
+//            AuthenticationProperties properties = CreateProperties(user.UserName);
+  //          AuthenticationTicket ticket = new AuthenticationTicket(oAuthIdentity, properties);
+      //      context.Validated(ticket);
+    //        context.Request.Context.Authentication.SignIn(cookiesIdentity);
         }
 
         public override Task TokenEndpoint(OAuthTokenEndpointContext context)
